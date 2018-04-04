62f9fc6078d2fcafef4dc855d7adcd8b6acfa48e
pereztjacob <pereztjacob@gmail.com>
2018-04-04 11:23:06 -0700

    function in-memory save, remove, get, getall

---
+++ test/in-mem.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const imports = require('../lib/in-mem');
+
+const { apple, pear, objArray } = imports;
+
+describe('in-memory db', () => {
+    
+    it('save', () => {
+        assert.equal(apple.save(), apple);
+        assert.equal(pear.save(), pear);
+    });
+
+    it('get', () => {
+        assert.equal(apple.get(), apple);
+    });
+
+    it('get all', () => {
+        assert.equal(objArray, apple.getAll());
+    })
+
+    it('remove',() => {
+        apple.remove();
+        assert.equal(apple.remove()[1], null);
+    })
+});
